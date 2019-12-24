d2ba269ef0de82ec2f0c43e632e500a8e2fef518
Joe Corapi <joe.corapi@smoothstack.com>
2019-12-23 23:06:12 -0500

    add table header and row styling

--- pagination.js
+++ pagination.js
@@ -18,32 +18,60 @@ for (let i=0; i<numOfBooks; i++) {
 
 let table = document.getElementById("table");
 
+let tHead = document.createElement('thead');
+let tRow = document.createElement('tr');
+tRow.className='d-flex justify-content-around bg-primary text-white'
+tHead.appendChild(tRow);
+
+let thID = document.createElement('th');
+thID.className='col-lg-4';
+thID.innerText='Book ID';
+tRow.appendChild(thID);
+
+let thTitle = document.createElement('th');
+thTitle.className='col-lg-4';
+thTitle.innerText='Book Title';
+tRow.appendChild(thTitle);
+
+let thAuthor = document.createElement('th');
+thAuthor.className='col-lg-4';
+thAuthor.innerText='Author Name';
+tRow.appendChild(thAuthor);
+
+table.appendChild(tHead);
+
+let tableBody=document.createElement('tbody');
+table.append(tableBody);
+
 let showPage = () => {
-    while (table.firstChild) {
-        table.removeChild(table.firstChild)
+    while (tableBody.firstChild) {
+        tableBody.removeChild(tableBody.firstChild)
     }
 
     for (let i=booksPerPage*pageNumber, j=i+booksPerPage; i<j; i++) {
         let row = document.createElement('tr')
-        row.className='row';
+        row.className='flex-row d-flex justify-content-around';
+        if (i%2==1){
+            row.className+=' bg-light'
+        }
 
         let bookId = document.createElement('td');
-        bookId.className='col align-stretch';
+        bookId.className='col-lg-4';
         bookId.innerText=bookList[i].bookId;
 
         let title = document.createElement('td');
-        title.className='col align-stretch';
+        title.className='col-lg-4';
         title.innerText=bookList[i].title;
 
         let authorName = document.createElement('td');
-        authorName.className='col align-stretch';
+        authorName.className='col-lg-4';
         authorName.innerText=bookList[i].authorName;
 
         row.appendChild(bookId);
         row.appendChild(title);
         row.appendChild(authorName);
 
-        table.appendChild(row)
+        tableBody.appendChild(row)
     }
 }
 
